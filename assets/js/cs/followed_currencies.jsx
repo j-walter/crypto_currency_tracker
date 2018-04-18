9ce301b67a4fef34a0a313e9bcfaef723e5a3ed0
jsemeraro <julia.semeraro@outlook.com>
2018-04-18 10:44:32 -0400

    made followed currencies a class instead of a function

--- assets/js/cs/followed_currencies.jsx
+++ assets/js/cs/followed_currencies.jsx
@@ -4,24 +4,39 @@ import Bitcoin from './bitcoin';
 import Ethereum from './ethereum';
 import Litecoin from './litecoin';
 
-export default function FollowedCurrencies(params) {
-  let prices = params.prices;
-  //   let tasks = _.map(params.tasks, (pp) => (<Task key={pp.id} task={pp} />));
-  // here we need to get the list from params and find out what cryptos the
-  // user is tracking, then make a call through the api
-  console.log("BTC", params.prices.btc.sell);
-  let bitcoin_price = params.prices.btc.sell;
-  let ethereum_price = params.prices.eth.sell;
-  let litecoin_price = params.prices.ltc.sell;
-  let coins = (
-    <div className="row cryto-container">
-      <Bitcoin price={bitcoin_price} />
-      <Litecoin price={litecoin_price} />
-      <Ethereum price={ethereum_price} />
-    </div>);
+export default class FollowedCurrencies extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  getPrice(currency) {
+    if (currency && currency.sell && currency.sell.current) {
+      return currency.sell.current;
+    } else {
+      return 0;
+    }
+  }
+
+  render() {
+    console.log(this.props.prices);
+    //   let tasks = _.map(params.tasks, (pp) => (<Task key={pp.id} task={pp} />));
+    // here we need to get the list from params and find out what cryptos the
+    // user is tracking, then make a call through the api
+    console.log("BTC", this.props.prices.btc);
+    let bitcoin_price = this.getPrice(this.props.prices.btc);
+    let ethereum_price = this.getPrice(this.props.prices.eth);
+    let litecoin_price = this.getPrice(this.props.prices.ltc);
+    let coins = (
+      <div className="row cryto-container">
+        <Bitcoin price={bitcoin_price} />
+        <Litecoin price={litecoin_price} />
+        <Ethereum price={ethereum_price} />
+      </div>);
+
     return (
       <div className="coins-div">
         {coins}
       </div>
     );
   }
+}
