f7b93114d9ba0624d7d48ea394b1e4862857ea28
baileylayton <bailey.layton.kay@gmail.com>
2018-04-17 22:39:13 -0400

    test

--- lib/crypto_currency_tracker/api.ex
+++ lib/crypto_currency_tracker/api.ex
@@ -8,36 +8,6 @@ defmodule CryptoCurrencyTracker.Api do
   @digital_currencies ApiAgent.digital_currencies()
 
 
-  # if currency_id is nil => return all the currency data that the active user is tracking
-  # otherwise return the specific data associated with the requested currency id
-
-  # if user details is nil
-  def get_currency(currency_id, user_details) when is_nil(currency_id) or currency_id in @digital_currencies do
-        if user_details  do
-      if currency_id in @digital_currencies do
-        %{currency_id => ApiAgent.get(currency_id)}
-      else
-        prices = %{}
-        Enum.reduce(@digital_currencies, prices, fn currency, prices ->
-          if Map.get(user_details, String.to_atom("follow_" <> currency))do
-            Map.put(prices, currency, ApiAgent.get(currency))
-          else 
-            prices
-          end
-        end)
-      end
-    else
-      if currency_id in @digital_currencies do
-        %{currency_id => ApiAgent.get(currency_id)}
-      else 
-        prices = %{}
-        Enum.reduce(@digital_currencies, prices, fn currency, prices ->
-          Map.put(prices, currency, ApiAgent.get(currency))
-        end)
-      end 
-    end
-
-  end
 
   def get_currency_pricing(currency_id, start_date, end_date) when currency_id in @digital_currencies do
     #assuming data format right now
